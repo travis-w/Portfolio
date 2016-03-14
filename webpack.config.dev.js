06a638c7bf89bf2b5923d5b7e331bce97934a0f2
Travis Weidenbenner <tjw8d3@mst.edu>
2016-03-14 17:19:24 -0500

    Set up webpack to generate build folder completely

---
+++ webpack.config.dev.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/main.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Travis Weidenbenner',
+      filename: 'index.html',
+      favicon: 'src/img/favicon.ico',
+      template: 'src/templates/index.jade'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+      {
+        test: /\.(png|jpg|woff|woff2|eot|ttf|svg|ico)(\?.*$|$)/,
+        loader: 'url',
+        query: {
+          limit: "100000",
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.jade$/,
+        loader: 'jade'
+      }
+    ]
+  },
+};
