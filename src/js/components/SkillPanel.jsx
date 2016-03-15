fcbd06b8a335d38394a81bc29f77772c1d8bef9b
Travis Weidenbenner <tjw8d3@mst.edu>
2016-03-15 14:02:46 -0500

    Fix linting issues in SkillPanel

--- src/js/components/SkillPanel.jsx
+++ src/js/components/SkillPanel.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-//Skills Component
-class SkillPanel extends React.Component {
-  render() {
-    return (
-      <div className="project">
-        <h1>{this.props.title}</h1>
-        {
-          this.props.skillList.map(function(skill) {
-            return <span key={skill} className="skill">{skill}</span>
-          })
-        }
-      </div>
-    )
-  }
-}
+// Skills Component
+const SkillPanel = ({ title, skillList }) => (
+  <div className="project">
+    <h1>{title}</h1>
+    {
+      skillList.map((skill) => (
+        <span key={skill} className="skill">{skill}</span>
+      ))
+    }
+  </div>
+);
+
+// Prop Types for SkillPanel
+SkillPanel.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  skillList: React.PropTypes.array.isRequired,
+};
 
 export default SkillPanel;
