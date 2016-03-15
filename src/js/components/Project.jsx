ed86a0a90c8a7aa65f7b80d30cf3e8edc7ddce7b
Travis Weidenbenner <tjw8d3@mst.edu>
2016-03-15 13:29:27 -0500

    Fix linting issues in Project

--- src/js/components/Project.jsx
+++ src/js/components/Project.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 
-//Display A Single Project
-class Project extends React.Component {
-  render() {
-    return (
-      <div className="project">
-        <h1>{this.props.project.title}</h1>
-        {this.props.project.description}
-        <ul className="project-links">
-        {
-          this.props.project.links.map((link) => {
-            return <li key={link.title}><a href={link.url} key={link.title} target="_blank">{link.title}</a></li>
-          })
-        }
-        </ul>
-      </div>
-    )
-  }
-}
+// Display A Single Project
+const Project = ({ project }) => (
+  <div className="project">
+    <h1>{project.title}</h1>
+    {project.description}
+    <ul className="project-links">
+    {
+      project.links.map((link) => (
+        <li key={link.title}>
+          <a
+            href={link.url}
+            key={link.title}
+            target="_blank"
+          >
+            {link.title}
+          </a>
+        </li>
+      ))
+    }
+    </ul>
+  </div>
+);
+
+// Prop Types for Project
+Project.propTypes = {
+  project: React.PropTypes.object.isRequired,
+};
 
 export default Project;
