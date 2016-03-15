5aa9ca11ab533bef9d74c20f990ab959cb2d2391
Travis Weidenbenner <tjw8d3@mst.edu>
2016-03-15 14:10:18 -0500

    Fix linting issues in SocialMedia

--- src/js/components/SocialMedia.jsx
+++ src/js/components/SocialMedia.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 
-//Social Media Component
-class SocialMedia extends React.Component {
-  render() {
-    return (
-      <div className="project-list">
-        <div className="project">
-          <h1>Social Media</h1>
-          <div className="social-media">
-          {
-            this.props.links.map(function(link) {
-              var glyphClass = "social " + link.glyphicon;
-              return <a href={link.url} key={link.title} title={link.title} target="_blank"><span className={glyphClass}></span></a>
-            })
-          }
-          </div>
-        </div>
+// Social Media Component
+const SocialMedia = ({ links }) => (
+  <div className="project-list">
+    <div className="project">
+      <h1>Social Media</h1>
+      <div className="social-media">
+      {
+        links.map((link) => {
+          const glyphClass = `social ${link.glyphicon}`;
+          return (
+            <a
+              href={link.url}
+              key={link.title}
+              title={link.title}
+              target="_blank"
+            >
+              <span className={glyphClass}></span>
+            </a>
+          );
+        })
+      }
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+);
+
+// Prop Types for SocialMedia
+SocialMedia.propTypes = {
+  links: React.PropTypes.array.isRequired,
+};
 
 export default SocialMedia;
