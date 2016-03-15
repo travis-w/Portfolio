cdd62e058d2adc0378c90f8f0c288357c482c78e
Travis Weidenbenner <tjw8d3@mst.edu>
2016-03-15 14:14:31 -0500

    Webpack config files bide by airbnb style guide

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,26 +1,26 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/main.jsx',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'js/main.js'
+    filename: 'js/main.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Travis Weidenbenner',
       filename: 'index.html',
       favicon: 'src/img/favicon.ico',
-      template: 'src/templates/index.jade'
+      template: 'src/templates/index.jade',
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
-        warnings: false
-      }
+        warnings: false,
+      },
     }),
-    new webpack.optimize.OccurenceOrderPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
   ],
   module: {
     loaders: [
@@ -29,32 +29,32 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
-        }
+          presets: ['es2015', 'react'],
+        },
       },
       {
         test: /\.scss$/,
-        loader: 'style!css!sass'
+        loader: 'style!css!sass',
       },
       {
         test: /\.(png|jpg|woff|woff2|eot|ttf|svg|ico)(\?.*$|$)/,
         loader: 'url',
         query: {
-          limit: "100000",
-          name: '[name].[ext]'
-        }
+          limit: '100000',
+          name: '[name].[ext]',
+        },
       },
       {
         test: /\.jade$/,
         loader: 'jade',
         query: {
-          pretty: true
-        }
+          pretty: true,
+        },
       },
       {
         test: /\.json$/,
-        loader: 'json'
-      }
-    ]
+        loader: 'json',
+      },
+    ],
   },
 };
