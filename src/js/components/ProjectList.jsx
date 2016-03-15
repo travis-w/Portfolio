9ca843c039f97830153e9f1c2d33a14e91d5f06b
Travis Weidenbenner <tjw8d3@mst.edu>
2016-03-15 13:32:11 -0500

    Fix linting issues in ProjectList

--- src/js/components/ProjectList.jsx
+++ src/js/components/ProjectList.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Project from './Project.jsx';
 
-//Display all projects
-class ProjectList extends React.Component {
-  render() {
-    return (
-      <div className="project-list">
-      {
-        this.props.projects.map((project) => {
-          return <Project key={project.title} project={project} />
-        })
-      }
-      </div>
-    )
+// Display all projects
+const ProjectList = ({ projects }) => (
+  <div className="project-list">
+  {
+    projects.map((project) => (
+      <Project key={project.title} project={project} />
+    ))
   }
-}
+  </div>
+);
+
+// Prop Types for Project List
+ProjectList.propTypes = {
+  projects: React.PropTypes.array.isRequired,
+};
 
 export default ProjectList;
