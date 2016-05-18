73898a90a6f39a65326f8130da337d1735fe7653
Travis Weidenbenner <tjw8d3@mst.edu>
2016-05-18 18:32:02 -0500

    Add rest of projects

--- src/Components/Data.elm
+++ src/Components/Data.elm
@@ -29,7 +29,7 @@ pPortfolio =
   , tools = ["NodeJS", "ReactJS", "Webpack", "HTML", "SASS"]
   , links =
     [ { title = "Link"
-      , url = "https://github.com/travis-w/chehttp://thetravisw.com"
+      , url = "http://thetravisw.com"
       , glyphicon = Nothing
       }
     , { title = "Source"
@@ -39,10 +39,70 @@ pPortfolio =
     ]
   }
 
+pSearchTwitter : Project
+pSearchTwitter =
+  { title = "Search On Twitter"
+  , description = "Twitter is the best source for information on current events.  Search On Twitter is a Google Chrome extension that allows you to search Twitter at any time. Just higlight what you want to search and you will have the option to search Twitter for the highlighted text in the context menu."
+  , tools = ["JavaScript", "Google Chrome API"]
+  , links =
+    [ { title = "Chrome Web Store"
+      , url = "https://chrome.google.com/webstore/detail/search-on-twitter/dhpmpdpphfgejncefefmdhklfbliefkm"
+      , glyphicon = Nothing
+      }
+    , { title = "Source"
+      , url = "https://github.com/travis-w/Search-On-Twitter"
+      , glyphicon = Nothing
+      }
+    ]
+  }
+
+pLinkSharing : Project
+pLinkSharing =
+  { title = "Link Sharing"
+  , description = "A Google Chrome extension that allows multiple users to share links between eachother."
+  , tools = ["JavaScript", "Google Chrome API"]
+  , links =
+    [ { title = "Chrome Web Store"
+      , url = "https://chrome.google.com/webstore/detail/link-sharing/haehadeohhghodnfohinokocaaellnga"
+      , glyphicon = Nothing
+      }
+    , { title = "Source"
+      , url = "https://github.com/travis-w/Link-Sharing"
+      , glyphicon = Nothing
+      }
+    ]
+  }
+
+pTwitchTetris : Project
+pTwitchTetris =
+  { title = "Twitch Plays Tetris"
+  , description = "Twitch Plays Tetris is an experiment in which Tetris is played by a group of viewers collaborating through a livestream chat. We used JS Tetris for the actual game and I created the functionality to interact with the chatroom."
+  , tools = ["JavaScript", "HTML", "CSS"]
+  , links =
+    [ { title = "Link"
+      , url = "http://twitch.tv/tetrisplaystwitch"
+      , glyphicon = Nothing
+      }
+    ]
+  }
+
+pCampusAlly : Project
+pCampusAlly =
+  { title = "CampusAlly"
+  , description = "CampusAlly that will allow students to collaborate on and share notes for classes. The development has currently been put on hold while I expand my knowledge of web development."
+  , tools = ["PHP", "HTML", "CSS", "MySQL"]
+  , links =
+    [ { title = "Link"
+      , url = "http://campusally.com"
+      , glyphicon = Nothing
+      }
+    ]
+  }
+
 pageProjects : Page
 pageProjects =
   { title = "Projects"
-  , projects = Just [pCheckers, pPortfolio]
+  , projects = Just [pCheckers, pPortfolio, pSearchTwitter, pLinkSharing, pTwitchTetris, pCampusAlly]
   , languages = Nothing
   , frameworks = Nothing
   , jobs = Nothing
