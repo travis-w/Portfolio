8d486f2de042a0f10b8c9862e954cfd8fa0eea91
Travis Weidenbenner <tjw8d3@mst.edu>
2016-05-19 20:56:31 -0500

    Add production webpack config and update readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Attempting to recreate my portfolio in Elm in order to learn the language.
+## Travis Weidenbenner Portfolio [Elm]
+[Link to Portfolio](http://thetravisw.com)
+
+My personal portfolio to display all of my projects and list previous employment
+information.
+
+##### Build
+* npm run dev - Development build (JS not minified)
+* npm run build - Production build (JS minified)
+
+##### Implementations
+In order to expand my knowledge and learn new frameworks/languages, I will be
+attempting to implement my portfolio in many different frameworks/languages.
+So far I have completed the following.
+
+* [React](https://github.com/travis-w/Portfolio/tree/master)
+* [Elm](https://github.com/travis-w/Portfolio/tree/elm-lang)
---
+++ webpack.config.prod.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/main.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Travis Weidenbenner',
+      filename: 'index.html',
+      favicon: 'src/img/favicon.ico',
+      template: 'src/templates/index.jade',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.elm$/,
+        loader: 'elm-webpack',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass',
+      },
+      {
+        test: /\.(png|jpg|woff|woff2|eot|ttf|svg|ico)(\?.*$|$)/,
+        loader: 'url',
+        query: {
+          limit: '100000',
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.jade$/,
+        loader: 'jade',
+        query: {
+          pretty: true,
+        },
+      },
+    ],
+  },
+};
